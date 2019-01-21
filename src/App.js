794dd5755c6239240f56f34540fe13a0bbf77df5
Aram Mikayelyan <aram.miqayelyan@gmail.com>
2019-01-21 18:18:50 +0400

    news app with react hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
+
+  const getResults = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const handleSearch = e => {
+    e.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  };
+
+  return (
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="React Logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-bold">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-2">
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          ref={searchInputRef}
+          className="border p-1 rounded"
+        />
+        <button type="submit" className="bg-orange rounded m-1 p-1">
+          Search
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="bg-teal text-white p-1 rounded"
+        >
+          Clear
+        </button>
+      </form>
+
+      {loading ? (
+        <div className="font-bold text-green-dark">Loading Results...</div>
+      ) : (
+        <ul className="list-reset leading-normal">
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a
+                href={result.url}
+                className="text-indigo-dark hover:text-indigo-darkest"
+              >
+                {result.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
+  );
 }
-
-export default App;
